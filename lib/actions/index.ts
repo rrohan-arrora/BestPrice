2f6dc189b7f938acf8283c8aab0bba7ca823fab9
Aaksh Gadekar <an14092000@gmail.com>
2023-10-06 02:22:55 +0530

    added email to product in order to track the product called the function from the modal.tsx component

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -5,6 +5,7 @@ import Product from "../models/product.model";
 import connectToDB from "../mongoose";
 import { scrapAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+import { User } from "@/types";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -86,7 +87,21 @@ export async function otherTrendingProducts(productId: string){
     }
 }
 
-export async function addUserEmailToProduct(productId: string, email:string){
+export async function addUserEmailToProduct(productId: string, userEmail:string){
+    try {
+        const product = await Product.findById(productId);
     
+        if(!product) return;
+    
+        const userExists = product.users.some((user: User) => user.email === userEmail);
+    
+        if(!userExists) {
+          product.users.push({ email: userEmail });
+    
+          await product.save();
+        }
+      } catch (error) {
+        console.log(error);
+      }
 }
 
