c8c57dda7ab502de1f62fbae25f97c36f433dfb2
Aaksh Gadekar <an14092000@gmail.com>
2023-10-06 03:12:02 +0530

    Added node mailer functionality Added various types used in creating the mail

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,6 +6,7 @@ import connectToDB from "../mongoose";
 import { scrapAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
+import { generateEmailBody, sendEmail } from "@/nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
     if(!productUrl) return;
@@ -99,6 +100,10 @@ export async function addUserEmailToProduct(productId: string, userEmail:string)
           product.users.push({ email: userEmail });
     
           await product.save();
+
+          const emailContent = await generateEmailBody(product, "WELCOME");
+
+          await sendEmail(emailContent, [userEmail]);
         }
       } catch (error) {
         console.log(error);
--- types/index.ts
+++ types/index.ts
@@ -23,3 +23,19 @@ export type Product = {
     isOutOfStock: Boolean;
     users?: User[];
 };
+
+export type NotificationType =
+  | "WELCOME"
+  | "CHANGE_OF_STOCK"
+  | "LOWEST_PRICE"
+  | "THRESHOLD_MET";
+
+export type EmailContent = {
+  subject: string;
+  body: string;
+};
+
+export type EmailProductInfo = {
+  title: string;
+  url: string;
+};
