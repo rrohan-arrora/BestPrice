65b6832a2b56c6020d6f6e70921abc3950ec6ef4
Aaksh Gadekar <an14092000@gmail.com>
2023-10-06 16:46:04 +0530

    fixed build error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -9,7 +9,7 @@ export const maxDuration = 10; // This function can run for a maximum of 300 sec
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export async function GET(){
+export async function GET(request: Request){
     try{
         connectToDB();
 
