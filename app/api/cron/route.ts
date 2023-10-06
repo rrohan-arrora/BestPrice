a204066828c2770f8b1debc4d219ad2ec9728bfa
Aaksh Gadekar <an14092000@gmail.com>
2023-10-06 14:19:13 +0530

    fixed build error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { generateEmailBody, sendEmail } from "@/nodemailer";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET(){
     try{
         connectToDB();
